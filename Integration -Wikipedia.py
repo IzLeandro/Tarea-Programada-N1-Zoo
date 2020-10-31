852ca9e920ac242595c4f53096001ba7d28195e9
IzLeandro <34462612+IzLeandro@users.noreply.github.com>
2020-10-31 13:18:56 -0600

    Added new files and some code added to integration wikipedia
    needs comments

---
+++ Integration -Wikipedia.py
@@ -0,0 +1,22 @@
+import wikipedia
+wikipedia.set_lang("ES")
+def getInfo(animal):
+    x=wikipedia.page(animal)
+    info=[animal, x.title,x.url,cleanText(wikipedia.summary(animal)),"La imagen puede ser encontrada aquí: " + x.images[0]]
+    print(info)
+    return ""
+
+def cleanText(text):
+    save=""
+    flag=0
+    for i in range(len(text)):
+        if flag!=0:
+            flag-=1
+            continue
+        if text[i]=="[": 
+            flag=2
+            continue
+        save=save+text[i]
+    return save
+  
+getInfo("Oso Panda")
