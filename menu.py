d7ddbe5727020f523a6257b1895e73a77bff231e
IzLeandro <34462612+IzLeandro@users.noreply.github.com>
2020-11-02 20:08:59 -0600

    nothing(really)

--- menu.py
+++ menu.py
@@ -1,11 +1,11 @@
 from files import cargarListaOriginal, readTxtFirstTime, grabar
-archivo=input("Inserte el directorio donde se encuentra el archivo o si se encuentra en la misma carpeta, el nombre: ")
-animales=cargarListaOriginal(archivo)
-if animales==[]:
+archivo = input("Inserte el directorio donde se encuentra el archivo o si se encuentra en la misma carpeta, el nombre: ")
+animales = cargarListaOriginal(archivo)
+if animales == []:
     print("ENTRAAAAAAAAAAAAAAAAAA")
-    cantAnimales=eval(input("digite la cantidad de animales que desea obtener del archivo: "))
-    animales= readTxtFirstTime(cantAnimales)
-
+    cantAnimales = eval(input("digite la cantidad de animales que desea obtener del archivo: "))
+    animales = readTxtFirstTime(cantAnimales)
+    #* re.match("^\d{1,}$",num) falta comprobar con 0
 
 #!AL cerrar archivo o al abrir el archivo; guardar
-grabar(archivo,animales)
+grabar(archivo, animales)
